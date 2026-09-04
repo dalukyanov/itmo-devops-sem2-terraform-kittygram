60157733028b36f69a5441ff6ce5f5cd50411d4f
dalukyanov <hanatsu.tori@gmail.com>
2026-09-04 21:10:58 +0300

    Fix: backend static 01

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 
 from django.contrib import admin
 from django.urls import include, path
-from django.shortcuts import redirect
+from django.http import JsonResponse
 
 from cats.views import AchievementViewSet, CatViewSet
 
@@ -11,11 +11,25 @@ router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
+
+def home(request):
+    """Простая страница приветствия для корневого пути"""
+    return JsonResponse({
+        'message': 'Welcome to Kittygram API!',
+        'endpoints': {
+            'api': '/api/',
+            'admin': '/admin/',
+            'api_cats': '/api/cats/',
+            'api_users': '/api/users/',
+        },
+        'documentation': 'https://github.com/dalukyanov/itmo-devops-sem2-terraform-kittygram'
+    })
+
+
 urlpatterns = [
-    # Редирект с корневого пути на API
-    path('', lambda request: redirect('/api/')),
+    path('', home, name='home'),  # Корневой путь возвращает JSON
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
 ]
